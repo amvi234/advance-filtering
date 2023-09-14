2b2971bfa4c9ab08043bff9ddb990df3ae7343d5
amvi234 <amitvikram.workspace@gmail.com>
2023-09-14 12:34:18 +0530

    [refactor] card component item

--- src/Products/Product.js
+++ src/Products/Product.js
@@ -1,30 +1,11 @@
-import { AiFillStar } from "react-icons/ai";
-import { BsFillBagHeartFill } from "react-icons/bs";
+import Card from "../components/Card";
 import "./Product.css";
 
 function Product() {
   return (
     <>
       <section className="card-container">
-        <section className="card">
-          <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="Shoe" className="card-img"/>
-          <div className="card-details">
-            <h3 className="card-title">
-              Shoe
-            </h3>
-            <section className="card-reviews">
-              <AiFillStar className="ratings-start"/> <AiFillStar className="ratings-start"/> <AiFillStar className="ratings-start"/> <AiFillStar className="ratings-start"/>
-              <span className="total-reviews">4</span>
-            </section>
-            <section className="card-price"></section>
-            <div className="price">
-              <del>$300</del> 200
-            </div>
-         <div className="bag">
-          <BsFillBagHeartFill className="bag-icon"/>
-         </div>
-          </div>
-        </section>
+     <Card/>
       </section>
     </>
   );
