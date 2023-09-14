5295f207dfb83497ac6bb5cc8ab98a62db0e3622
amvi234 <amitvikram.workspace@gmail.com>
2023-09-14 17:36:50 +0530

    [feat] declared other states and functions for handling changes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,64 @@ import Navigation from "./Navigation/Nav";
 import Recommended from "./Recommended/Recommended";
 import Product from "./Products/Product";
 import Sidebar from "./Sidebar/Sidebar";
+import { useState } from "react";
+import products from './db/data'
+import Card from "./components/Card";
+import category from "./Sidebar/Category/Category";
 
 function App() {
-  return (
-  <>
-  <Sidebar/>
-  <Navigation/>
-  <Recommended/>
-  <Product/>
-  </>
-)}
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [query, setQuery] = useState("");
 
-export default App;
+  const handleInputCHange = event => {
+    setQuery(event.target.value)
+  }
+
+  const filteredItems = products.filter(product => product.title.
+    toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1))
+
+  const handleChange = event => {
+    setSelectedCategory(event.target.value)
+  }
+
+  const handleClick = event => {
+    setSelectedCategory(event.target.value);
+  }
+
+  function filteredData(products, selected, query) {
+    let filteredProducts = products
+    if (query) {
+      filteredProducts = filteredItems
+    }
+
+    // Selected Filter
+
+    if (selected) {
+      filteredProducts = filteredProducts.filter(({
+        category, color, company,
+        newPrice, title
+      }) => category === selected || color === selected || company === selected || newPrice === selected || title === selected);
+    }
+    return filteredProducts.map(({img, title, star, reviews, newPrice, prevPrice}) =>(
+      <Card 
+      key = {Math.random()}
+      img={img}
+      title={title}
+      star={star}
+      reviews={reviews}
+      newPrice={newPrice}
+      prevPrice={prevPrice}
+      />
+    ))
+  }
+
+    return (
+      <>
+        <Sidebar />
+        <Navigation />
+        <Recommended />
+        <Product />
+      </>
+    );
+}
+  export default App;
