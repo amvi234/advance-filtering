c74c1ce37e93b91bf29477fa088415f10570ccfb
amvi234 <amitvikram.workspace@gmail.com>
2023-09-14 22:39:01 +0530

    [fix] styling of Category component

--- src/Sidebar/Category/Category.js
+++ src/Sidebar/Category/Category.js
@@ -1,38 +1,36 @@
 import "./Category.css";
+import Input from "../../components/input";
 
 function category({handleChange}) {
-  return (
-    <div>
+  return <div>
       <h2 className="sidebar-title">Category</h2>
       <div>
         <label className="sidebar-label-container">
-          <input onChange={handleChange} type="radio" value="" name="test"></input>
+          <input onChange={handleChange} type="radio" value="" name="test"/>
           <span className="checkmark"></span>All
         </label>
-
-        <input handleChange={handleChange}
+        <Input handleChange={handleChange}
         value="sneakers"
         title="Sneakers"
         name="test"
         />
-        <input handleChange={handleChange}
+        <Input handleChange={handleChange}
         value="sandals"
         title="Sandals"
         name="test"
         />
-        <input handleChange={handleChange}
+        <Input handleChange={handleChange}
         value="flats"
         title="Flats"
         name="test"
         />
-        <input handleChange={handleChange}
+        <Input handleChange={handleChange}
         value="heels"
         title="Heels"
         name="test"
         />
       </div>
     </div>
-  );
 }
 
 export default category;
